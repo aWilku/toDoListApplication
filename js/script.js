72c54c7aee3a5a3a0f4c194145e093e31f24146d
Adrian Wilk <wilku5991@wp.pl>
2021-05-03 21:35:43 +0200

    rendering view and adding new task

--- js/script.js
+++ js/script.js
@@ -10,9 +10,38 @@
         },
     ];
 
+    const onFormSubmit =(event) => {
+        event.preventDefault();
+
+        const newTask = document.querySelector(".js-newTask").value.trim();
+        if(newTask === ""){
+            return;
+        }
+        tasks.push(
+            {
+                content: newTask,
+                done: false,
+            }
+        );
+        render();
+    }
+
+    const render = () => {
+        let htmlString = "";
+        for(const task of tasks){
+            htmlString += `
+            <li class="taskList__element${task.done ? " taskList__element--done" : ""}" >
+            ${task.content}
+            </li>
+            `;
+        }
+        document.querySelector(".js-taskList").innerHTML = htmlString;
+    }
 
     const init = () => {
-
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", onFormSubmit)
+        render();
     };
     init();
 }
