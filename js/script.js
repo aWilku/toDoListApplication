284fad04887ede69011f64fd56543a3a3932546a
Adrian Wilk <wilku5991@wp.pl>
2021-05-03 19:42:31 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+{
+    const tasks = [
+        {
+            content: "Zadanie testowe numer 1",
+            done: false,
+        },
+        {
+            content: "Zadanie testowe numer 2",
+            done: true,
+        },
+    ];
+
+
+    const init = () => {
+
+    };
+    init();
+}
